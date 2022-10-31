7741b41d371082e1b3d08f5a9a872d6e4ea548a8
Svyatoslav <pozolotin-cvat@yandex.ru>
2022-11-01 00:56:59 +0300

    вводим два фора и считаем символы

--- code/Program.cs
+++ code/Program.cs
@@ -1,2 +1,10 @@
 ﻿string[] a = {"Sun", "Monday", "Tuersday",
       "Wednesday", "Thirsday", "F", "Saturday"};
+Console.Write("[");      
+for(int i = 0; i < a.Length; i++){
+    int count = 0;
+    for(int j = 0; j < a[i].Length; j++){
+        
+        count = count + 1;
+        //Console.Write("Длина" + count);
+    }
