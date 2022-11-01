a16e18b80210b17eddefa209eb7b1458bf73aeb6
Svyatoslav <pozolotin-cvat@yandex.ru>
2022-11-01 20:08:20 +0300

    вывод через массив

--- code/Program.cs
+++ code/Program.cs
@@ -1,6 +1,8 @@
 ﻿string[] a = {"Sun", "Monday", "Tuersday",
       "Wednesday", "Thirsday", "F", "Saturday"};
+string [] b = new string [a.Length];
 Console.Write("[");      
+int k = -1;    
 for(int i = 0; i < a.Length; i++){
     int count = 0;
     for(int j = 0; j < a[i].Length; j++){
@@ -9,8 +11,15 @@ for(int i = 0; i < a.Length; i++){
         //Console.Write("Длина" + count);
     }
     if (count < 4){
-        Console.Write($" {a[i]}, ");
+        k++;
+        b[k] = a[i];
     }
     
+}
+for(int i = 0; i < k; i++){
+    Console.Write(b[i] + ", ");
+}
+for(int i = k; i < k+1; i++){
+    Console.Write(b[i]);
 }
 Console.Write("]");
